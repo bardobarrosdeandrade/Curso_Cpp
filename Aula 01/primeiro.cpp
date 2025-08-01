f1ee3966ad55c612c07973c13898c6037049a93d
Bardo Barros <152213482+bardobarrosdeandrade@users.noreply.github.com>
2025-07-31 23:46:51 -0300

    Add files via upload

---
+++ Aula 01/primeiro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Ola, mundo\n";
+    std::cout << "Boa tarde!" << std::endl;
+
+    int idade; float altura;
+    std::cout << "Qual sua idade? ";
+    std::cin >> idade;
+
+    std::cout << "Qual sua altura? ";
+    std::cin >> altura;
+
+    return 0;
+}
