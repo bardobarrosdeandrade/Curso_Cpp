f727d5bb332d4eec85b249d92ecb338752120625
Bardo Barros <152213482+bardobarrosdeandrade@users.noreply.github.com>
2025-08-02 23:14:46 -0300

    Update primeiro.cpp

--- Aula 01/primeiro.cpp
+++ Aula 01/primeiro.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
-
-int main(){
-    std::cout << "Ola, mundo\n";
-    std::cout << "Boa tarde!" << std::endl;
-
-    int idade; float altura;
-    std::cout << "Qual sua idade? ";
-    std::cin >> idade;
-
-    std::cout << "Qual sua altura? ";
-    std::cin >> altura;
-
+#include <locale.h>  // necessário para setlocale()
+using namespace std;
+/*
+para criar o arquivo executavel do codigo (compilar), p executar corretamente :
+no terminal do codigo, serve para compilar : .\nomeDoArquivo.cpp -o nomeDoarquivo
+e no terminal mesmo é só executar chamando o proprio nome com ./nomeDoarquivo.exe que ele vai ser executado
+*/ 
+/*
+no c++ para atualizar o codigo de fato, tem que fazer (CTRL + SHIFT B) aí sim ele vai executar corretamente
+*/
+/*int main() {
+    std::cout <<"Hello, world!\n";
+    std::cout <<"Curso de c++" << std::endl;
+    std::cout <<"Oxetech Lab";
+}
+*/
+ // cout imprime os dados / saida de dados
+ // cin ler os dados / entrada de dados
+int main() {
+    setlocale(LC_ALL, "");  // Ativa locale do sistema (Windows) para suportar acentos
+    int x;
+    cout << "digite sua idade";
+    cin >> x;
+    cout << "sua idade é : " << x << endl;
     return 0;
 }
